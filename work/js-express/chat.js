8aff3d449b4ef39ca76f310c8604127a15854e26
EmmaXi <xxmhzau@gmail.com>
2020-01-31 14:25:37 -0800

    js-express

--- work/js-express/chat.js
+++ work/js-express/chat.js
@@ -1,22 +1,10 @@
 const chat = {
     users: {
-        Bob: 'Bob',
-        Emma: 'Emma',
+        
     },
 
     messages: [
-        {
-            sender: 'Bob',
-            timestamp: new Date(),
-            text: 'Do you have lunch?'
-        },
-
-        {
-            sender: 'Emma',
-            timestamp: new Date(),
-            text: 'No, still learning INFO 6250.'
-
-        }
+        
     ],
 
     addMessage: function({ sender, text, timestamp = new Date() }) {
