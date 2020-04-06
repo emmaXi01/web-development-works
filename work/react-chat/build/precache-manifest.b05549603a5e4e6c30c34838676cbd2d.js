92dce23db1194a539ceb6cbf6da21d4e230728dc
EmmaXi <xxmhzau@gmail.com>
2020-04-06 14:48:15 -0700

    push build file

---
+++ work/react-chat/build/precache-manifest.b05549603a5e4e6c30c34838676cbd2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7d85681bc912133ad857424cd2ca8b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "985df19fde990ad307b3",
+    "url": "/static/css/main.5fbe8bc7.chunk.css"
+  },
+  {
+    "revision": "6283dadaf347e30fcfc6",
+    "url": "/static/js/2.ad5cd774.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ad5cd774.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "985df19fde990ad307b3",
+    "url": "/static/js/main.2eee99c8.chunk.js"
+  },
+  {
+    "revision": "bd4a7141480c08d4636f",
+    "url": "/static/js/runtime-main.2ce54bef.js"
+  }
+]);
